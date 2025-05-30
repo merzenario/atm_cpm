b8f18b79cc858a70687b2761a1e1256743dcf72c
merzenario <estebanmolina159@gmail.com>
2025-05-29 23:47:14 -0500

    Preparacion final del entregable

--- OneDrive/Escritorio/construccion de sof/atmCpm/src/main/java/com/banco_cpm/atmCpm/entity/AccountType.java
+++
@@ -1,7 +0,0 @@
-package com.banco_cpm.atmCpm.entity;
-
-public enum AccountType {
-    SAVINGS,
-    CURRENT,
-
-}
--- OneDrive/Escritorio/construccion de sof/atmCpm/src/main/java/com/banco_cpm/atmCpm/entity/MotionType.java
+++
@@ -1,9 +0,0 @@
-package com.banco_cpm.atmCpm.entity;
-
-public enum MotionType {
-    CONSULT,
-    WITHDRAWAL,
-    TRANSFER,
-    DEPOSIT,
-
-}
